96cf7fcb6087c5d8d5d03dbdf7880feae0f44e82
Lims <swoo0514lim@gmail.com>
2025-08-14 02:13:50 +0900

    Feat: model initDB() 개발

--- src/models/index.js
+++ src/models/index.js
@@ -1,1 +1,10 @@
 const sequelize = require('../../config/database');
+const Product = require('./Product');
+
+async function initDB({ alter = false, force = false } = {}) {
+  await sequelize.authenticate();
+  await sequelize.sync({ alter, force });
+  return { sequelize, Product };
+}
+
+module.exports = { sequelize, Product, initDB };
