a4f601268ea07565c78ef383c8f060a85ff20edf
Lims <swoo0514lim@gmail.com>
2025-08-17 17:33:22 +0900

    Feat: 상태 전이 라우터 정의

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/order.controller');
 const sessionAuth = require('../middlewares/sessionAuth');
+const adminAuth = require('../middlewares/adminAuth');
 
 router.post('/', sessionAuth, orderController.createOrder);
+
+router.patch('/:id/status', adminAuth, orderController.updateStatus);
 
 module.exports = router;
