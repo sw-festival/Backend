13d34077a72512a4be80e74561680c6d82be6fe9
Lims <swoo0514lim@gmail.com>
2025-08-31 11:32:52 +0900

    Feat: GET /orders 라우터 정의 - 주방 페이지에서 사용할 주문 정보 조회 라우터

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -8,4 +8,6 @@ router.post('/', sessionAuth, orderController.createOrder);
 
 router.patch('/:id/status', adminAuth, orderController.updateOrderStatus);
 
+router.get('/', adminAuth, orderController.getOrdersForKitchen);
+
 module.exports = router;
